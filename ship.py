6fb6d02cfb2588672c9b102f356c61c576b00039
nielssei <107681969+nielssei@users.noreply.github.com>
2022-06-25 12:51:17 +0200

    Update and rename ship to ship.py

--- ship.py
+++ ship.py
@@ -1,7 +1,11 @@
+# Klasse initialisiert Schiffe
 class Ship:
     def __init__(self, origin, direction, size):
         self.size = size
+        # Koordinaten werden hier gespeichert:
         self.coordinates = []
+        # Ursprungskoordinate wird um so viele Koordinaten erweitert, wie groß das Schiff ist
+        # in die ausgewählte Richtung. (0: Norden, 1: Osten, 2: Süden, 3: Westen)
         for k in range(size):
             if direction == 0:
                 self.coordinates.append((origin[0] - k, origin[1]))
@@ -11,3 +15,4 @@ class Ship:
                 self.coordinates.append((origin[0] + k, origin[1]))
             if direction == 3:
                 self.coordinates.append((origin[0], origin[1] - k))
+            
