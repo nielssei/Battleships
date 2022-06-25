f5518456e7150d139d230454e56b4b0acc7754c1
nielssei <107681969+nielssei@users.noreply.github.com>
2022-06-25 18:00:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -97,12 +97,10 @@ def place_ships(board, health, name, number):
 def handleIncomingMove():
 
     print(f"Waiting for {remoteName}")
-    letter_to_num = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10,
-                     'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10}
     # receive and process remote bomb
     bomb = server.receive()
     bombCoordinates = bomb.split(",")
-    result = board.bomb(letter_to_num[bombCoordinates[0]] - 1, int(bombCoordinates[1]) - 1)
+    result = board.bomb(int(bombCoordinates[0]) - 1, int(bombCoordinates[1]) - 1)
 
     # send replies to remote player
     client.send(result)
@@ -116,11 +114,11 @@ def handleIncomingMove():
 
 # handles outgoing game move
 def handleOutgoingMove():
-    letter_to_num = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10,
-                     'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10}
+    letter_to_num = {'A': '1', 'B': '2', 'C': '3', 'D': '4', 'E': '5', 'F': '6', 'G': '7', 'H': '8', 'I': '9', 'J': '10',
+                     'a': '1', 'b': '2', 'c': '3', 'd': '4', 'e': '5', 'f': '6', 'g': '7', 'h': '8', 'i': '9', 'j': '10'}
     # send bomb to remote player
     bomb_ = input("Send bomb (e.G. A4): ")
-    bomb = (letter_to_num[bomb_[0]] + int(bomb_[1])).split(",")
+    bomb = (letter_to_num[bomb_[0]] + "," + bomb_[1])
     # TODO: validate the input
     client.send(bomb)
 
